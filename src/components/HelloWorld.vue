e830533ea7819646341fb84fb2e17c6a6df7d779
rayray <571386342@qq.com>
2023-08-27 15:01:32 +0800

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const tabs = await chrome.tabs.query({
+  url: [
+    "https://developer.chrome.com/docs/webstore/*",
+    "https://developer.chrome.com/docs/extensions/*",
+  ],
+});
+</script>
+
+<template>
+  <div class="greetings">
+
+  </div>
+</template>
+
+<style scoped>
+</style>
