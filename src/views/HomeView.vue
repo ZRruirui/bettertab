052ea7cdec578054051936d9df07356576e22070
rayray <571386342@qq.com>
2023-08-27 17:30:27 +0800

    feat: add tab link

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
+import {onMounted, Ref, ref} from "vue";
+import Tab = chrome.tabs.Tab;
+
+const tabs: Ref<Tab[]> = ref([])
+const addr = window.location.href
+onMounted(async () => {
+   tabs.value = await chrome.tabs.query({});
+})
+console.log(tabs)
 
 </script>
 
 <template>
-  <div>hello world</div>
+  <div>
+    {{JSON.stringify((tabs))}}}
+    {{addr}}
+    <li v-for="tab in tabs" :key="tab.id">{{tab.title}}</li>
+  </div>
 </template>
+
+<style scoped>
+</style>
